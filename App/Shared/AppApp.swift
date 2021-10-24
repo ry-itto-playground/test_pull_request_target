798acb65138a72627d636d738360f75b821f6cc3
ryoya ito <110ryoya@gmail.com>
2021-10-24 21:03:37 +0900

    Init

---
+++ App/Shared/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  Shared
+//
+//  Created by 伊藤凌也 on 2021/10/24.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
